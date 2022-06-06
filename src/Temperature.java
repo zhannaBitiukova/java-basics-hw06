e86fbad14d4dfd869ac8e79ab84d1d92dddd7b18
ZhannaBit <zhanna.bitiukova@gmail.com>
2022-06-06 21:31:28 +0200

    Implemented, tests passed

--- src/Temperature.java
+++ src/Temperature.java
@@ -7,8 +7,7 @@ public class Temperature {
    * @return temperature degree in Celsius
    */
   public static double fahrenheitToCelsius(double fahrenheit) {
-    // TODO fill in code here and replace the return statement
-    return 0;
+    return 5 * (fahrenheit - 32) / 9;
   }
 
   /**
@@ -18,7 +17,6 @@ public class Temperature {
    * @return temperature degree in Fahrenheit
    */
   public static double celsiusToFahrenheit(double celsius) {
-    // TODO fill in code here and replace the return statement
-    return 0;
+    return (9 * celsius + 160) / 5;
   }
 }
--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,9 @@ public class TemperatureTest {
     return new Object[][]{
         {212.0, 100.0},
         {32.0, 0.0},
-        {-15, -26.11111111111111}
-        // TODO add 2 more test data here
+        {-15, -26.11111111111111},
+        {-459.66999999999996, -273.15},
+        {-40, -40}
     };
   }
 
@@ -28,8 +29,9 @@ public class TemperatureTest {
     return new Object[][]{
         {100.0, 212.0},
         {0.0, 32.0},
-        {-26.11111111111111, -15}
-        // TODO add 2 more test data here
+        {-26.11111111111111, -15},
+        {-273.15, -459.66999999999996},
+        {-40, -40}
     };
   }
 
